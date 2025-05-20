fe30fa6429541c40700477b2bbb25adb318a9ddc
KAntigua <karenantigua31@gmail.com>
2025-05-20 09:56:46 -0700

    Primer commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color:rgb(166, 255, 178);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        h1 {
+            font-size: 2.5em;
+            color: #333;
+            background-color:rgb(218, 26, 131);
+            border-radius: 5px;
+            margin-bottom: 20px;
+
+        }
+        .menu {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            background-color:rgb(236, 98, 174);
+            padding: 20px;
+            border-radius: 15px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+        a {
+            text-decoration: none;
+            color: black;
+            background-color:rgb(255, 255, 255);
+            padding: 5px 20px;
+            text-align: center;
+            border-radius: 8px;
+            transition: background-color 0.3s ease;
+            display: inline-block;
+            margin-bottom: 10px;
+        }
+     </style>
+
+
+</head>
+<body>
+    
+<div> id = "mitarea1">
+
+    <div>
+        <h1>Bienvenidos a mi Menu </h1>
+        
+    <div>
+    <div class="menu">
+    <ul>
+         <li>
+            <a href="tarjeta.php">Mi Tarjeta</a>
+         </li>
+         <li>
+            <a href="calculadora.php">Calculadora</a>
+         </li>
+         <li>
+            <a href="adivina.php">Adivina el Numero</a>
+         </li>
+         <li>
+            <a href="acerca_de.php">Acerca de</a>
+         </li>
+    </ul>
+     </div>
+       
+
+<div>
+
+</body>
+</html>
